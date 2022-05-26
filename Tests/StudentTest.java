8516b0926b315304bf9d7ffc4a3df2e2dd47dd60
Louis Tatlock <tatl0008@flinders.edu.au>
2022-05-26 13:14:32 +0930

    git commit -m "EA-6 Began testing of the Student class testing the set and get methods as well as starting to look at the results methods.

--- Tests/StudentTest.java
+++ Tests/StudentTest.java
@@ -1,41 +1,61 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
 
+    static Student student;
+
+    @BeforeAll
+    static void before() {student = new Student("test,98575,Test,First,TheWorld");}
+
+
     @Test
     void setStudentID() {
+        student.setStudentID("95535");
+        assertEquals("95535",student.getStudentID());
     }
 
     @Test
     void setFamilyName() {
+        student.setFamilyName("Jeffries");
+        assertEquals("Jeffries",student.getFamilyName());
     }
 
     @Test
     void setGivenName() {
+        student.setGivenName("Jack");
+        assertEquals("Jack",student.getGivenName());
     }
 
     @Test
     void getStudentID() {
+        assertEquals("98575",student.getStudentID());
     }
 
     @Test
     void getFamilyName() {
+        assertEquals("Test",student.getFamilyName());
     }
 
 
     @Test
     void getGivenName() {
+        assertEquals("First",student.getGivenName());
     }
 
-
+    //WIP
     @Test
+    @BeforeAll
     void addResult() {
+        student.addResult("COMP1000,HD,100");
+        assertEquals("testing", student.getResult("TheWorld"));
     }
-
+    //WIP
     @Test
     void getResults() {
+        assertEquals("testing", student.getResult("TheWorld"));
     }
 
     @Test
